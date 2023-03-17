651cb9d0c86e0c7c225fda85acb25e8d55eb2c3e
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2023-03-17 17:24:45 -0300

    feat: requisito 3 resolvido

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import index from './pages/index';
+import index from './pages/Index';
+import shoppingcart from './pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ index } />
+          <Route exact path="/" component={ index } />
+          <Route exact path="/shoppingcart" component={ shoppingcart } />
         </Switch>
       </BrowserRouter>
     );
