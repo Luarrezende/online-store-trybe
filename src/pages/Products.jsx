aa0f6ab102a15223496cd9e941295a00c57d5949
Karla Keurin <karlakeurinoliveira@gmail.com>
2023-03-22 20:50:27 -0300

    Resolve requisito 9

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,6 +6,7 @@ import { getProductById } from '../services/api';
 class Products extends React.Component {
   state = {
     productsDetail: {},
+    productsToCart: JSON.parse(localStorage.getItem('cart')) || [],
   };
 
   async componentDidMount() {
@@ -15,6 +16,35 @@ class Products extends React.Component {
       productsDetail: apiResult,
     });
   }
+
+  handleCartList = () => {
+    const { productsToCart, productsDetail } = this.state;
+    this.setState({
+      productsToCart: [...productsToCart, productsDetail],
+    });
+  };
+
+  saveToCart = () => {
+    const { productsToCart, productsDetail } = this.state;
+    const { id } = productsDetail;
+    const toCart = productsToCart.find((item) => item.id === id);
+
+    if (toCart) {
+      const updateProduct = productsToCart.filter((itens) => itens.id !== id);
+      productsDetail.quantity += 1;
+      // console.log(id.quantity);
+      const productQuantity = [...updateProduct, productsDetail.quantity];
+      localStorage.setItem('cart', JSON.stringify(productQuantity));
+      this.setState({
+        productsToCart: productQuantity,
+      });
+    } else {
+      console.log('else');
+      productsDetail.quantity = 1;
+      localStorage.setItem('cart', JSON.stringify([...productsToCart, productsDetail]));
+      this.handleCartList(id);
+    }
+  };
 
   render() {
     const { productsDetail } = this.state;
@@ -29,11 +59,16 @@ class Products extends React.Component {
           alt={ title }
         />
         <p data-testid="product-detail-price">{ price }</p>
-        <Link data-testid="shopping-cart-button" to="/shoppingcart">
-          <button
-            type="button"
-          >
-            Adicionar oo carrinho
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => this.saveToCart(productsDetail) }
+        >
+          Adicionar ao carrinho
+        </button>
+        <Link to="/shoppingcart">
+          <button type="button" data-testid="shopping-cart-button">
+            Carrinho de compras
           </button>
         </Link>
       </>
